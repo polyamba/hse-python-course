fbb7e7fc8f972e90d76549be6cf975bd99cd6ecb
Polina Zhilina <113125577+polyamba@users.noreply.github.com>
2025-10-11 00:43:57 +0300

    Update and rename api.py to seminars/seminar1.py

---
+++ seminars/seminar1.py
@@ -0,0 +1,33 @@
+import json
+
+class Dataset:
+    def __init__(self, filename:str):
+        self.filename = filename
+        self.data = self._read_file()
+        self.song_dict = {song['id']: [song['title'], song['similar_ids']] for song in self.data} if self.data else {}
+
+    def _read_file(self):
+        """Читает JSON-файл и возвращает данные в виде списка словарей"""
+        try:
+            with open(self.filename, 'r') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            print("Error: The file 'data.json' was not found.")
+        except Exception as e:
+            print(f'Ошибка: {e}')
+
+    def get_song(self, n:int):
+        """Возвращает название нужной песни по её ID"""
+        return self.song_dict.get(n)[0]  
+            
+    def find_similar(self, n:int):
+        """Находит и выводит названия песен, похожих на заданную по ID"""
+        similar_songs = self.song_dict.get(n)[1]
+        for i in similar_songs:
+            print(self.song_dict.get(i)[0])
+                    
+if __name__ == '__main__':
+    dataset = Dataset('api.json')
+    print(dataset.data[:5])
+    print(dataset.get_song(3))
+    dataset.find_similar(3)
