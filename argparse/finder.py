8f87f0bcc4cd0e0acba2393480f821774d5c4c8d
Polina Zhilina <113125577+polyamba@users.noreply.github.com>
2025-12-09 19:18:03 +0300

    Update finder.py

--- argparse/finder.py
+++ argparse/finder.py
@@ -1,7 +1,4 @@
-# Задача: Создать скрипт поиска файлов в папке по расширению
-# Использование:
-#   python finder.py /path/to/search --ext txt pdf
-# Параметры: путь, расширения (nargs='+')
+
 
 import argparse
 from pathlib import Path
@@ -19,5 +16,3 @@ for e in args.ext:
     for p in Path(args.path).rglob(pattern = f'*.{e}'):
         if p is not None:
             print(p)
-
-# проверить существование пути
